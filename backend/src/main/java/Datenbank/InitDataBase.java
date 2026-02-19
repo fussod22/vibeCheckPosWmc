c060ef603eb01ad52be4706c60884b3b20c08e17
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 18:47:05 +0100

    add annoatations to initDataBase

---
+++ backend/src/main/java/Datenbank/InitDataBase.java
@@ -0,0 +1,15 @@
+package Datenbank;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class InitDataBase {
+
+    public void initDataBase(){
+
+        try{
+
+        }
+    }
+}
