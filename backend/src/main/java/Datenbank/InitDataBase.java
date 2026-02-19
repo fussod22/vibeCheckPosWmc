d275d728a2e3dc92b9522f2dd71c636e03521477
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 18:48:50 +0100

    implemented initDataBase for Artists

--- backend/src/main/java/Datenbank/InitDataBase.java
+++ backend/src/main/java/Datenbank/InitDataBase.java
@@ -1,15 +1,35 @@
 package Datenbank;
 
+import Pojos.Artist;
+import Repositorys.ArtistRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import javax.xml.stream.util.EventReaderDelegate;
+import java.io.InputStream;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class InitDataBase {
 
+    ArtistRepository artistRepository;
     public void initDataBase(){
 
-        try{
+        try {
+            InputStream inputStream = this.getClass()
+                    .getResourceAsStream("/artist.json");
 
+            ObjectMapper objectMapper = new ObjectMapper()
+                    .registerModule(new JavaTimeModule());
+
+            List<Artist> students = objectMapper
+                    .readerForListOf(Artist.class)
+                    .readValue(inputStream);
+
+
+            artistRepository.saveAll(students);
         }
     }
 }
