fcf5adb316b299473c69947a55b591408af6697c
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 18:57:27 +0100

    implement initDataBaseEvent

--- backend/src/main/java/Datenbank/InitDataBase.java
+++ backend/src/main/java/Datenbank/InitDataBase.java
@@ -1,7 +1,9 @@
 package Datenbank;
 
 import Pojos.Artist;
+import Pojos.Event;
 import Repositorys.ArtistRepository;
+import Repositorys.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,7 +18,8 @@ import java.util.List;
 public class InitDataBase {
 
     private final ArtistRepository artistRepository;
-    public void initDataBase(){
+    private final EventRepository eventRepository;
+    public void initDataBaseArtist(){
 
         try {
             InputStream inputStream = this.getClass()
@@ -25,12 +28,32 @@ public class InitDataBase {
             ObjectMapper objectMapper = new ObjectMapper()
                     .registerModule(new JavaTimeModule());
 
-            List<Artist> students = objectMapper
+            List<Artist> artists = objectMapper
                     .readerForListOf(Artist.class)
                     .readValue(inputStream);
 
 
-            artistRepository.saveAll(students);
+            artistRepository.saveAll(artists);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void initDataBaseEvent(){
+
+        try {
+            InputStream inputStream = this.getClass()
+                    .getResourceAsStream("/event.json");
+
+            ObjectMapper objectMapper = new ObjectMapper()
+                    .registerModule(new JavaTimeModule());
+
+            List<Event> events = objectMapper
+                    .readerForListOf(Event.class)
+                    .readValue(inputStream);
+
+
+            eventRepository.saveAll(events);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
