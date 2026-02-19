61e810a80f4ef0ae12e51ed06ea9caa63170db33
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 16:54:40 +0100

    add annotations Rating

--- backend/src/main/java/Pojos/Rating.java
+++ backend/src/main/java/Pojos/Rating.java
@@ -1,7 +1,13 @@
 package Pojos;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rating {
 }
