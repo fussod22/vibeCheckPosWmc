bf8d4a9cabf4f03605cb59a42596957497398763
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 16:42:27 +0100

    create Artist id

--- backend/src/main/java/Pojos/Artist.java
+++ backend/src/main/java/Pojos/Artist.java
@@ -1,7 +1,20 @@
 package Pojos;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
