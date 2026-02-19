3602e2f204e880d9071675ea00f9774c69b7dc7a
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 18:39:10 +0100

    create get Route for all Artists

--- backend/src/main/java/Controller/ArtistController.java
+++ backend/src/main/java/Controller/ArtistController.java
@@ -4,6 +4,7 @@ import Pojos.Artist;
 import Services.ArtistService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class ArtistController {
 
     ArtistService artistService;
 
+    @GetMapping("/artists")
     public ResponseEntity<List<Artist>> getAllArtists(){
 
     }
