9758205f512aa73927a54e53f3cd180ae56f2733
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 18:38:32 +0100

    create getAllArtists in Controller

--- backend/src/main/java/Controller/ArtistController.java
+++ backend/src/main/java/Controller/ArtistController.java
@@ -1,14 +1,21 @@
 package Controller;
 
+import Pojos.Artist;
 import Services.ArtistService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/api")
-
 public class ArtistController {
 
     ArtistService artistService;
+
+    public ResponseEntity<List<Artist>> getAllArtists(){
+
+    }
 
 }
