df50cc4507731bddef079d481d823541ed217ac3
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 20:25:26 +0100

    add route with params to get artist

--- backend/src/main/java/org/example/backend/Controller/ArtistController.java
+++ backend/src/main/java/org/example/backend/Controller/ArtistController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,7 +25,7 @@ public class ArtistController {
     }
 
     @GetMapping("/artists")
-    public ResponseEntity<Artist> getArtist(){
+    public ResponseEntity<Artist> getArtist(@RequestParam Long id){
 
     }
 
