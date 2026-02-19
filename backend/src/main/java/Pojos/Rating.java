cd02b9362c6db2273e48500453e910e2d5da1bab
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 16:55:47 +0100

    add rating id

--- backend/src/main/java/Pojos/Rating.java
+++ backend/src/main/java/Pojos/Rating.java
@@ -1,6 +1,9 @@
 package Pojos;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
