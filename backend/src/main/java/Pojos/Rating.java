50321d3a8072802f9f0c1dfe37971c8080791c4e
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 16:57:06 +0100

    add rating creadedAt;

--- backend/src/main/java/Pojos/Rating.java
+++ backend/src/main/java/Pojos/Rating.java
@@ -8,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -21,4 +23,6 @@ public class Rating {
     private Integer stars;
 
     private String comment;
+
+    private LocalDateTime createdAt;
 }
