ed97333a230aa0883c8e19b241c652985cdc3d45
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 18:36:59 +0100

    add Annotations ArtistController

--- backend/src/main/java/Controller/ArtistController.java
+++ backend/src/main/java/Controller/ArtistController.java
@@ -1,4 +1,12 @@
 package Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+
 public class ArtistController {
+
+
 }
