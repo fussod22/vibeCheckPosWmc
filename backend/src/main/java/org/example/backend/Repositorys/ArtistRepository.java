c79de30aaf5eb9d7ec22ce795a3ad58cf98b62b3
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 19:16:38 +0100

    add findByFirstnameAndLastname to ArtistRepository

--- backend/src/main/java/org/example/backend/Repositorys/ArtistRepository.java
+++ backend/src/main/java/org/example/backend/Repositorys/ArtistRepository.java
@@ -4,6 +4,11 @@ import org.example.backend.Pojos.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
+
+    Optional<Artist> findByFirstnameAndLastname(String firstname, String lastname);
+
 }
