d8f5c8596b95020c63e66d136a445770c38486bd
fussod22 <fussod22@htl-kaindorf.at>
2026-02-19 18:35:40 +0100

    create ArtistService with Annotations

--- backend/src/main/java/Services/ArtistService.java
+++ backend/src/main/java/Services/ArtistService.java
@@ -1,9 +1,12 @@
 package Services;
 
+import Repositorys.ArtistRepository;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @NoArgsConstructor
 public class ArtistService {
+
+    ArtistRepository artistRepository; 
 }
