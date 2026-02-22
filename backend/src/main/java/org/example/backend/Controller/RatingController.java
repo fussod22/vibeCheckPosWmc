17514d3b668b9c5d32a785ed6089359065b68d4c
fussod22 <fussod22@htl-kaindorf.at>
2026-02-22 19:53:24 +0100

    implement getRatingByEventID in RatinController

--- backend/src/main/java/org/example/backend/Controller/RatingController.java
+++ backend/src/main/java/org/example/backend/Controller/RatingController.java
@@ -2,9 +2,15 @@ package org.example.backend.Controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.Pojos.Rating;
 import org.example.backend.Services.RatingService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api")
@@ -12,4 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RatingController {
 
     private final RatingService ratingService;
+
+    @GetMapping("/event/{eventId}")
+    public ResponseEntity<List<Rating>> getRatingsByEvent(@PathVariable Long eventId) {
+
+    }
 }
