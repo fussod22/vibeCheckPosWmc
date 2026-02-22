55374622281a9e5dd46cd9377030bf7d6b21946a
fussod22 <fussod22@htl-kaindorf.at>
2026-02-22 19:51:15 +0100

    implement getRatingsByEvent in Service

--- backend/src/main/java/org/example/backend/Services/RatingService.java
+++ backend/src/main/java/org/example/backend/Services/RatingService.java
@@ -1,12 +1,22 @@
 package org.example.backend.Services;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.Pojos.Rating;
+import org.example.backend.Repositorys.EventRepository;
 import org.example.backend.Repositorys.RatingRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RatingService {
 
     private final RatingRepository ratingRepository;
+    private final EventRepository eventRepository;
+
+    public List<Rating> getRatingsByEvent(Long id) {
+        List<Rating> ratings = ratingRepository.findByEventId(id);
+        return ratings;
+    }
 }
