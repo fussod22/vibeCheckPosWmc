06c9beb81947c7205291b2068840843aa791d45b
fussod22 <fussod22@htl-kaindorf.at>
2026-02-22 18:42:13 +0100

    set event controller annotations

---
+++ backend/src/main/java/org/example/backend/Controller/EventController.java
@@ -0,0 +1,11 @@
+package org.example.backend.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class EventController {
+}
