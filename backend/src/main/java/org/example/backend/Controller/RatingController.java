07aa679e353c5b6eaf71ee0bed463000370d92b5
fussod22 <fussod22@htl-kaindorf.at>
2026-02-22 19:54:43 +0100

    implement addRating in Controller

--- backend/src/main/java/org/example/backend/Controller/RatingController.java
+++ backend/src/main/java/org/example/backend/Controller/RatingController.java
@@ -6,9 +6,7 @@ import org.example.backend.Pojos.Rating;
 import org.example.backend.Services.RatingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,10 @@ public class RatingController {
     public ResponseEntity<List<Rating>> getRatingsByEvent(@PathVariable Long eventId) {
         List<Rating> ratings = ratingService.getRatingsByEventId(eventId);
         return ResponseEntity.ok(ratings);
+    }
+
+    @PostMapping("/{eventId}")
+    public ResponseEntity<Rating> addRating(@PathVariable Long eventId, @RequestBody Rating rating) {
+
     }
 }
