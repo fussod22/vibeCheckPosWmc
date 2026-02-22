0fb8b68498083dd9ca6a5aa7b9b6caa910b76ad6
fussod22 <fussod22@htl-kaindorf.at>
2026-02-22 18:45:49 +0100

    implement getAllEvents in EventService

--- backend/src/main/java/org/example/backend/Services/EventService.java
+++ backend/src/main/java/org/example/backend/Services/EventService.java
@@ -1,12 +1,20 @@
 package org.example.backend.Services;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.Pojos.Event;
 import org.example.backend.Repositorys.EventRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
 
     private final EventRepository eventRepository;
+
+    public List<Event> getALlEvents(){
+        List<Event> events = eventRepository.findAll();
+        return events;
+    }
 }
