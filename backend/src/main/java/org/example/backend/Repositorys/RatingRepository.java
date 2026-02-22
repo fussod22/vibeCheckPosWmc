f3d7d02cf6a542104dc5d041c90f7e0a22b6b985
fussod22 <fussod22@htl-kaindorf.at>
2026-02-22 19:39:48 +0100

    create rating repo

---
+++ backend/src/main/java/org/example/backend/Repositorys/RatingRepository.java
@@ -0,0 +1,10 @@
+package org.example.backend.Repositorys;
+
+
+import org.example.backend.Pojos.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+}
