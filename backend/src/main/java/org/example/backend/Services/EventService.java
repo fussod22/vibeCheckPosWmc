90af5cdba8e0ab64b456a429196c987c676c68f2
fussod22 <fussod22@htl-kaindorf.at>
2026-02-22 18:44:47 +0100

    add eventrepo to eventservi

--- backend/src/main/java/org/example/backend/Services/EventService.java
+++ backend/src/main/java/org/example/backend/Services/EventService.java
@@ -1,9 +1,12 @@
 package org.example.backend.Services;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.Repositorys.EventRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
+
+    private final EventRepository eventRepository;
 }
