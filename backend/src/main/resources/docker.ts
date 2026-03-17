2ec5e3e731f8c5781f5aa8f49ff91861ad03af29
fussod22 <fussod22@htl-kaindorf.at>
2026-03-17 15:41:20 +0100

    Meine aktuellen Änderungen sichern

---
+++ backend/src/main/resources/docker.ts
@@ -0,0 +1,6 @@
+#docker build --taf ghcr-io/schatel/backend:latest ./target
+
+#--tag 'ghcr.io' -> URL of docker registry
+#     'schate' -> username of github
+#     'backend' -> image name
+#    ':latest' -> image version
